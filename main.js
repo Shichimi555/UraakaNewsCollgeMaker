c7eff470c419380bd4a9e34277244faf07a4445a
Shichimi555 <kttg1025531@gmail.com>
2023-03-07 23:20:09 +0900

    あとダウンロードするボタンだけ

--- main.js
+++ main.js
@@ -1,153 +1,32 @@
-// // fileInput.addEventListener('change', (event) => {
-// button.addEventListener("click",function(){
-
-//     const formElements = document.forms.sendform;
-//     // alert(formElements);
-//     formElements.text.value = '';
-//     console.log('Text: ', formElements.text.value);
-//     alert("sus")
-
-
-//     const file = event.target.files[0];
-//     const reader = new FileReader();
-//     reader.readAsDataURL(file);
-//     reader.onload = (event) => {
-//         const image = new Image();
-//         image.src = event.target.result;
-
-//         alert("hello")
-//         // 画像を合成する処理を実行する
-//         const background = document.getElementById('background');
-//         const overlay = document.createElement('img');
-//         overlay.src = event.target.result;
-//         overlay.addEventListener('load', () => {
-//             const canvas = document.createElement('canvas');
-//             canvas.width = background.width;
-//             canvas.height = background.height;
-//             const context = canvas.getContext('2d');
-//             context.drawImage(background, 0, 0);
-//             context.drawImage(overlay, 0, 0);
-//             const result = canvas.toDataURL('image/jpeg');
-//             // 合成した画像を表示するなどの処理を実行する
-
-//             const resultImage = document.createElement('img');
-//             resultImage.src = result;
-//             document.body.appendChild(resultImage);
-
-//         });
-
-//   };
-// });
-
-
 var form = document.getElementById('fileform');
 
 form.addEventListener('submit', function(event) {
   event.preventDefault(); // フォームが自動的に送信されるのを防止する
-  
-  var firstName = form.elements['firstName'].value;
-  var lastName = form.elements['lastName'].value;
-  var email = form.elements['email'].value;
-  
+
   var fileInput = form.elements['fileInput'];
   var file = fileInput.files[0]; // 選択されたファイルを取得する
-  var filer = fileInput.files;
 
-  var reader = new FileReader();
-  reader.readAsDataURL(file); // ファイルを読み込む
-  
-  reader.onload = function() {
-    var fileContent = reader.result;
-    var type = filer[0].type;
-
-    // ファイルの内容を使って何らかの処理を行う
-    console.log(firstName, lastName, email, fileContent);
-
-    var name = filer[0].name;
-    var size = filer[0].size;
-    var type = filer[0].type;
-
-    var items = [name,size,type];
-
-
-    if (type=="image/png"){
-      console.log("png");
-    }else if (type=="image/jpeg"){
-      console.log("jpeg")
-    }else{
-      console.log("error");
-      alert("pngかjpgファイルを指定してください")
-    }
-
-    var imgData = fileContent;
-    var img = new Image();
-
-
-    img.onload = function() {
-      try{
-        // var result_type = document.getElementById('details');
-        // result_type.remove();
-        var canvas = document.getElementById('preview'); //もともとの画像を削除
-        canvas.remove();
-        var background = document.getElementById('created')
-        background.remove()
-      }catch(Exception){
-        console.log(e)
-        console.log("新規");
-      }finally{
-        var div = document.createElement("div"); // div要素を作成
-
-
-        
-        
-        // for(let i = 0 ; i < items.length ; i++){
-        //   var p = document.createElement("p");
-        //   console.log(items[i]);
-        //   p.textContent = items[i]; // div要素にテキストを挿入
-        //   p.id = 'pre';
-        //   div.appendChild(p);
-        // }
-
-        // div.id = 'details'
-        // document.body.appendChild(div); // body要素の子要素としてdiv要素を追加
-
-        var canvas = document.createElement('canvas');
-        canvas.width = img.width;
-        canvas.height = img.height;
-        canvas.id = "preview";
-
-        var ctx = canvas.getContext('2d');
-        ctx.drawImage(img, 0, 0);
-
-        document.body.appendChild(canvas); // 画像を表示する
-      }
-
+  try{
+    var reader = new FileReader();
+    reader.readAsDataURL(file); // ファイルを読み込む
       
-    };
+  }catch(error){
+    if (error instanceof TypeError){
+      alert('画像を選択してください');
+      const inputfile = document.getElementById('InputFile');
+      inputfile.value = '';
+    }else{
+      console.log(error);
+      alert('エラーが発生しました。もう一度お試しください。')
+      const inputfile = document.getElementById('InputFile');
+      inputfile.value = '';
+    }
+    return;
+  }
 
-    img.src = imgData;
-
-    
-
-  };
-});
-
-
-
-form.addEventListener('submit', function(event) {
-  event.preventDefault(); // フォームが自動的に送信されるのを防止する
-  
-  var fileInput = form.elements['fileInput'];
-  var file = fileInput.files[0]; // 選択されたファイルを取得する
-  var filer = fileInput.files;
-
-  var reader = new FileReader();
-  reader.readAsDataURL(file); // ファイルを読み込む
-  
   reader.onload = function() {
     var fileContent = reader.result;
     var imgData = fileContent;
-
 
     try{
       var canvas = document.getElementById('created')
@@ -171,8 +50,65 @@ form.addEventListener('submit', function(event) {
       ctx.drawImage(backgroundImage, 0, 0);
       var x = 10;
       var y = 10;
+
+      try{
       ctx.drawImage(image, x, y);
+      }catch(error){
+        if (error instanceof DOMException){
+        console.log(error);
+        alert('pngファイルかjpgファイルを選択してください');
+        const inputfile = document.getElementById('InputFile');
+        inputfile.value = '';
+        }else{
+          console.log(error);
+          alert('エラーが発生しました。もう一度お試しください。');
+          const inputfile = document.getElementById('InputFile');
+          inputfile.value = '';
+        }
+      // ここで処理終了
+        return;
+      }
+
       document.body.appendChild(canvas);
+      
+      try{
+        var alreadyButton = document.getElementById('download');
+        alreadyButton.parentNode.removeChild(alreadyButton);
+      }catch(e){
+        ; // 無視
+
+      }
+
+      var againstForm = document.querySelector('.buttonform')
+      var download = document.createElement("button");
+      download.type = 'submit';
+      download.classList.add('input');
+      download.id = 'download';
+      download.textContent = 'Download';
+      againstForm.appendChild(download);
+
     };
   }
 });
+
+
+var buttons = document.getElementById("buttonform");
+
+buttons.addEventListener('submit', function(event){
+  event.preventDefault();
+  const buttonId = event.submitter.id; // クリックされたbuttonのidを取得
+  if (buttonId=='delete'){
+    try{
+      canvas = document.getElementById('created');
+      canvas.remove();
+    }catch(e){
+      alert('画像がありません');
+    }
+    return;
+  }else{
+    console.log('download');
+  }
+
+  
+});
+
